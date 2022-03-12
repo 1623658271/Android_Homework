1b4af87b3184c0a68a4333951f7660194a86113d
1623658271 <1623658271@qq.com>
2022-03-12 16:37:50 +0800

    直接套模板

---
+++ app/thefirsthomework/src/main/java/com/example/thefirsthomework/respository/MyRetrofit.kt
@@ -0,0 +1,11 @@
+package com.example.thefirsthomework.respository
+
+/**
+ * description ： TODO:类的作用
+ * author : lfy
+ * email : 1623658271@qq.com
+ * date : 2022/3/12 13:22
+ */
+class MyRetrofit {
+
+}
---
+++ app/thefirsthomework/src/main/java/com/example/thefirsthomework/view/IViewChange.kt
@@ -0,0 +1,11 @@
+package com.example.thefirsthomework.view
+
+/**
+ * description ： TODO:类的作用
+ * author : lfy
+ * email : 1623658271@qq.com
+ * date : 2022/3/12 13:21
+ */
+interface IViewChange {
+    fun setDataToTextView()
+}
